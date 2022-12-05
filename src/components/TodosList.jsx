045c50b9cc55ae2608f984bbcb3504f48465c4b7
jellywiz <muzaffer.dler@gmail.com>
2022-12-05 20:35:19 +0300

    Add todolist

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import React from "react";
+import TodosItem from "./TodosItem";
+
+class TodosList extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.todos.map(todo => (
+          <TodosItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default TodosList;
